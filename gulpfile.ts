ba8e51b97510ab84a39399d1797bf348e7ae7db6
xiaobin.kuang <xiaobin.kuang@hstong.com>
2021-07-07 21:08:01 +0800

    feat: 更改 Bulid Gulp 构建流程

--- gulpfile.ts
+++ gulpfile.ts
@@ -10,7 +10,6 @@ const start = cb => {
 const complete = cb => {
   logger.info('✨  Glup 构建完成 ~');
   cb();
-  process.exit(); // 结束 node 进程
 };
 
 export const changelog = series(start, changelogTask, complete);
@@ -21,4 +20,11 @@ export const changelog = series(start, changelogTask, complete);
 // 3. api-extractor 生成统一的声明文件, 删除多余的声明文件
 // 4. 完成
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const build = series(start, clearLibTask, buildRollupTask, apiExtractorTask, complete);
+export const build = series(
+  start,
+  clearLibTask,
+  buildRollupTask,
+  apiExtractorTask,
+  changelogTask,
+  complete
+);
